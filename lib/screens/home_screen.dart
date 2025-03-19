464154e0db593264f3d9ba6e469b273f33fbf747
Jacob Chippett <jchippet@uoguelph.ca>
2025-03-19 11:36:07 -0400

    feat: add main menu for navigation

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenScreenState();
+}
+
+class _HomeScreenScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('UoGuesser'),
+          backgroundColor: Colors.deepPurple,
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              ElevatedButton(
+                onPressed: () => context.go('/game_guessing_screen'),
+                child: Text('Daily Game'),
+              ),
+              ElevatedButton(
+                onPressed: () => context.go('/upload_picture_screen'), 
+                child: Text('Take Picture'),
+              ),
+              ElevatedButton(
+                onPressed: () {}, 
+                child: Text('TODO: find it mode'),
+              ),
+              ElevatedButton(
+                onPressed: () {}, 
+                child: Text('TODO: Leaderboard'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
